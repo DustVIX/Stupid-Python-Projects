2465eeb6653b8dbd06ef6be209ab214ba9683654
DustVIX <dustvix@gmail.com>
2025-04-09 01:32:03 +0300

    first commit

---
+++ Notifications.py
@@ -0,0 +1,12 @@
+from winotify import Notification, audio
+
+toast = Notification(
+    app_id="Windows Security", 
+    title="Windows Security",
+    msg="يا عبد",
+    icon=r"c:\Users\DustVIX\Downloads\windows.ico" 
+)
+
+toast.add_actions(label="موفق", launch="C:\\path\\to\\file.txt")
+toast.set_audio(audio.Default, loop=False)
+toast.show()
